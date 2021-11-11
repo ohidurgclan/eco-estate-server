a966769590b24c080798629c5f43477400d96034
ohidurgclan <ohidur.ru.2019@gmail.com>
2021-11-12 05:59:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5030;
+
+
+//Middle Ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mvbo5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('eco_estate');
+        const servicesCollection = database.collection('services');
+        const userCollection = database.collection('userservices');
+        
+        // Add Service
+        app.post('/services', async (req, res) => {
+            const serviceItem = req.body;
+            const result = await servicesCollection.insertOne(serviceItem);
+            console.log(result);
+            res.json(result)
+        });
+
+        // Get Service API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        });
+        // Find API
+        app.get("/userservice", async (req, res) => {
+        const cursor = userCollection.find({});
+        const orders = await cursor.toArray();
+        res.send(orders);
+        });
+        // Get Api by users email
+        app.get("/userservice/:email", async (req, res) => {
+        const cursor = userCollection.find({ email: req.params.email });
+        const orders = await cursor.toArray();
+        res.send(orders);
+        });
+        // Api Post
+        app.post("/userservice", async (req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.json(result);
+        });
+    
+        // Get a user data
+        app.get("/userservice/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await userCollection.findOne(query);
+        res.send(result);
+        });
+
+        // Update Data By User
+        app.put("/userservice/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedUser = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+            status: updatedUser.status,
+            },
+        };
+        const resut = await userCollection.updateOne(filter, updateDoc, options);
+        res.json(resut);
+        });
+        
+        // Delete Order
+        app.delete("/userservice/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
+
+        res.json(result);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Assignment Twelve Server Running');
+});
+
+app.listen(port, () => {
+    console.log("Example App Port", port)
+});
